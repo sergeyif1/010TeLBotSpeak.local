c99987624d8319a828e40eb9b1a3d398e7412d1d
sergeyif1 <sergeyif1@gmail.com>
2023-11-10 17:12:22 +0200

    10:18_10.11.2023

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
-console.log(" Hello!");
+import { Telegraf } from 'telegraf';
+import config from 'config';
+
+const bot = new Telegraf(config.get('TELEGRAM_TOKEN'));  //создаем бот, и в скобках передаем токен полученный у самого телеграмма
+
+bot.launch();  //запускаем бота
+
+process.once('SIGINT', () => bot. stop('SIGINT'));     // если вдруг нода завершается самовольно то тогда мы будем останавливать бота
+process.once('SIGTERM', () => bot. stop('SIGTERM'));   // сигналом завершения процесса (termination signal) в Unix-подобных операционных системах. Этот сигнал отправляется процессу, чтобы попросить его корректно завершить выполнение. 
+
